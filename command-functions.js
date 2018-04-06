ed22aa55006b50c701af3dfe536adc18682922a8
Chris Walker <cmw161@uclive.ac.nz>
2018-04-06 22:30:01 +1200

    Move some things around.

---
+++ command-functions.js
@@ -0,0 +1,40 @@
+const tagbro = require("./tagbro.js");
+const utils = require("./utils.js");
+const log = tagbro.log;
+const prefix = config.prefix;
+
+const axios = require("axios");
+
+function echo(message, command, argsString) {
+    log("Responding to " + prefix + command + ".");
+    if (message.author.tag === "Zagd#6682") {
+        message.delete().catch(O_o=>{});
+        message.channel.send(argsString);
+    }
+}
+
+function giveServerCounts(message, command) {
+    log("Responding to " + prefix + command + ".");
+    let serverCounts = "";
+    let count = 0;  // Amount of completed axios gets.
+    for (let i = 0; i < servers.length; i++) {
+        let server = tagbro.servers[i];
+        let address = tagbro.serverAddresses[server];
+        axios.get(address + "stats").then(function (response) {
+            count++;
+            let data = response.data;
+            serverCounts = serverCounts.concat(
+                utils.pad(" ".repeat(10), server + ":", false) +
+                utils.pad("00", data.players, true) +
+                " players and " +
+                utils.pad("00", data.games, true) +
+                " games.\n"
+            );
+            if (count === servers.length) {
+                message.channel.send("```" + serverCounts + "```");
+            }
+        }).catch(function (error) {
+            log(error);
+        });
+    }
+}
--- tagbro.js
+++ tagbro.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
-const axios = require("axios");
+const commands = require("./command-functions.js");
+const utils = require("./utils.js");
 const config = require("./config.json");
 const prefix = config.prefix;
 
@@ -15,23 +16,19 @@ bot.on("ready", function () {
 });
 
 bot.on("message", message => {
-    let regex = new RegExp("^" + prefix);
+    if (message.content.indexOf(config.prefix) !== 0) return;
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    const argsString = message.slice((prefix + command + " ").length);
+    const regex = new RegExp("^" + prefix);
+
     if (message.content === prefix + "server count" || message.content === prefix + "sc" ) {
-        giveServerCounts(message);
+        commands.giveServerCounts(message, command);
     }
-    let commandWithArgs = "echo";
-    if (new RegExp(regex.source.concat(commandWithArgs + " .+")).test(message.content)) {
-        let commandStart = prefix + commandWithArgs + " ";
-        let argsString = message.content.slice(message.content.indexOf(commandStart) + commandStart.length);
-        echo(message, argsString);
+    if (new RegExp(regex.source.concat("echo" + " .+")).test(message.content)) {
+        commands.echo(message, command, argsString);
     }
 });
-
-function echo(message, argsString) {
-    if (message.author.tag === "Zagd#6682") {
-        message.channel.send(argsString);
-    }
-}
 
 const servers = [
     "Diameter",
@@ -54,42 +51,6 @@ const serverAddresses = {
     "Orbit": "http://tagpro-orbit.koalabeast.com/",
     "Chord": "http://tagpro-chord.koalabeast.com/"
 };
-
-function giveServerCounts(message) {
-    log("Responding to " + prefix + "server count.");
-    let serverCounts = "";
-    let count = 0;  // Amount of completed axios gets.
-    for (let i = 0; i < servers.length; i++) {
-        let server = servers[i];
-        let address = serverAddresses[server];
-        axios.get(address + "stats").then(function (response) {
-            count++;
-            let data = response.data;
-            serverCounts = serverCounts.concat(
-                pad(" ".repeat(10), server + ":", false) +
-                pad("00", data.players, true) +
-                " players and " +
-                pad("00", data.games, true) +
-                " games.\n"
-            );
-            if (count === servers.length) {
-                message.channel.send("```" + serverCounts + "```");
-            }
-        }).catch(function (error) {
-            log(error);
-        });
-    }
-}
-
-function pad(pad, str, padLeft) {
-    if (typeof str === 'undefined')
-        return pad;
-    if (padLeft) {
-        return (pad + str).slice(-pad.length);
-    } else {
-        return (str + pad).substring(0, pad.length);
-    }
-}
 
 function log(message) {
     console.log(message);
