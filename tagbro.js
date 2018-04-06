dbda011be0a86e2cc6517d8a827800b8a5f44c1a
Chris Walker <cmw161@uclive.ac.nz>
2018-04-06 22:49:27 +1200

    Fix

--- tagbro.js
+++ tagbro.js
@@ -19,7 +19,7 @@ bot.on("message", message => {
     if (message.content.indexOf(config.prefix) !== 0) return;
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
-    const argsString = message.slice((prefix + command + " ").length);
+    const argsString = message.content.slice((prefix + command + " ").length);
     const regex = new RegExp("^" + prefix);
 
     if (message.content === prefix + "server count" || message.content === prefix + "sc" ) {
