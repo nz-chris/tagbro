29f702825e8fe039ac9929bd713ccd458ae0ec1c
Chris Walker <cmw161@uclive.ac.nz>
2018-04-10 16:57:05 +1200

    hi

--- src/commands.js
+++ src/commands.js
@@ -11,11 +11,6 @@ const commandsInfo = require("./commands-info.js");
 const config = require("../config.json");
 const prefix = config.prefix;
 
-// Map full command names to relevant functions.
-const commandFunctions = {
-    [commandsInfo.info.server_stats]: giveSortedServerStats,
-    [commandsInfo.info.rpugs_matchmaking]: giveRankedPugsMatchmakingLink
-};
 //TODO: incorporate private commands into this delegate, but still keep the actual functions in their own module.
 //tagbro.js shudnt have to import private-functions.js
 
@@ -43,6 +38,12 @@ giveSortedServerStats = async function(message) {
 
 giveRankedPugsMatchmakingLink = function(message) {
     message.channel.send("<" + constants.rpugsAddress + ">");
+};
+
+// Map full command names to relevant functions.
+const commandFunctions = {
+    [commandsInfo.info.server_stats]: giveSortedServerStats,
+    [commandsInfo.info.rpugs_matchmaking]: giveRankedPugsMatchmakingLink
 };
 
 //TODO: ..help
