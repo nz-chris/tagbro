89dfe70e7d80aabe1facbe78bddc4536f62eccb3
Chris Walker <cmw161@uclive.ac.nz>
2018-04-10 16:50:17 +1200

    BLA BLA BLA who cares

--- src/tagbro.js
+++ src/tagbro.js
@@ -25,8 +25,8 @@ bot.on("ready", function () {
 
 bot.on("message", message => {
     if (message.content.indexOf(config.prefix) !== 0) return;
-    if (!commands.delegateCommandMessage(message)) privateCommands.de;
-    else ()
+    if (!commands.delegateCommandMessage(message));
+    return;
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
     const argsString = message.content.slice((prefix + command + " ").length);
