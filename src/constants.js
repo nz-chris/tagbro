655f850e6d64a9fde86dd16df0902ddf45762d56
Chris Walker <cmwalker.nz@gmail.com>
2021-07-21 21:45:59 +1200

    YEEEEAAAAH BIOOOOIIIYYY

--- src/constants.js
+++ src/constants.js
@@ -1,33 +1,29 @@
 exports.servers = [
-    "Diameter",
-    "Centra",
-    "Sphere",
-    "Origin",
-    "Radius",
-    "Pi",
-    "Orbit",
-    "Chord",
-];
+    'Oceania',
+    'US West',
+    'US East',
+    'Europe',
+]
 
-exports.serverAddresses = {
-    "Diameter": "http://tagpro-diameter.koalabeast.com/",
-    "Centra": "http://tagpro-centra.koalabeast.com/",
-    "Sphere": "http://tagpro-sphere.koalabeast.com/",
-    "Origin": "http://tagpro-origin.koalabeast.com/",
-    "Radius": "http://tagpro-radius.koalabeast.com/",
-    "Pi": "http://tagpro-pi.koalabeast.com/",
-    "Orbit": "http://tagpro-orbit.koalabeast.com/",
-    "Chord": "http://tagpro-chord.koalabeast.com/"
-};
+exports.rpugsAddress = 'http://lejdesigns.com/rankedPUGs/matchmaking.php'
 
-exports.rpugsAddress = "http://lejdesigns.com/rankedPUGs/matchmaking.php";
+exports.oltpDiscId = '394459353060605953'
 
-exports.oltpDiscId = "394459353060605953";
+exports.tagbrobotChannelId = '431742487695065108'
 
-exports.tagbrobotChannelId = "431742487695065108";
+exports.serverStatsMessageId = '431827952892641280'
 
-exports.serverStatsMessageId = "431827952892641280";
+exports.zagdTag = 'Zagd#8966'
 
-exports.zagdTag = "Zagd#6682";
+exports.spaceZws = ' \u200B'
 
-exports.spaceZws = " \u200B";
+exports = {
+    tpa: 'https://tagpro.koalabeast.com/',
+
+    servers: [
+        'Oceania',
+        'US West',
+        'US East',
+        'Europe',
+    ],
+}
--- src/depths/server-stats.js
+++ src/depths/server-stats.js
@@ -2,135 +2,132 @@
  * Low level server count functionality.
  */
 
-const axios = require("axios");
+const axios = require('axios')
 
-const utils = require("../utils.js");
-const {log, err} = require("../utils.js");
-const constants = require("../constants.js");
-const commandsInfo = require("../commands-info.js");
+const utils = require('../utils.js')
+const { log, err } = require('../utils.js')
+const constants = require('../constants.js')
+const commandsInfo = require('../commands-info.js')
 
-const config = require("../../config.json");
+const config = require('../../config.json')
 
 module.exports = {
     getSortedServerStatsMessage: async function () {
-        let sortedServerStatsMessage = "";
-        let sortedServerStats = await getSortedServerStats();
-        let largestPlayerCountDigits = getLargestPlayerCount(sortedServerStats).toString().length;
-        let largestGameCountDigits = getLargestGameCount(sortedServerStats).toString().length;
+        let sortedServerStatsMessage = ''
+        let sortedServerStats = await getSortedServerStats()
+        let largestPlayerCountDigits = getLargestPlayerCount(sortedServerStats).toString().length
+        let largestGameCountDigits = getLargestGameCount(sortedServerStats).toString().length
         for (let i = 0; i < sortedServerStats.length; i++) {
             sortedServerStatsMessage = sortedServerStatsMessage.concat(
                 formatServerStats(sortedServerStats[i], largestPlayerCountDigits, largestGameCountDigits) +
-                " <" + constants.serverAddresses[sortedServerStats[i][0]] + ">"
-            );
+                ' <' + constants.serverAddresses[sortedServerStats[i][0]] + '>',
+            )
             if (i !== sortedServerStats.length - 1) {
-                sortedServerStatsMessage = sortedServerStatsMessage.concat("\n");
+                sortedServerStatsMessage = sortedServerStatsMessage.concat('\n')
             }
         }
-        return sortedServerStatsMessage;
+        return sortedServerStatsMessage
     },
 
     setUpIntervalUpdates: function (bot) {
-        let oltpGuild;
-        let tagbroBotChannel;
-        let existingServerStatsMessage;
+        let oltpGuild
+        let tagbroBotChannel
+        let existingServerStatsMessage
+        log(bot.guilds)
         if (bot.guilds.has(constants.oltpDiscId)) {
-            oltpGuild = bot.guilds.get(constants.oltpDiscId);
+            oltpGuild = bot.guilds.get(constants.oltpDiscId)
             if (oltpGuild.channels.has(constants.tagbrobotChannelId)) {
-                tagbroBotChannel = oltpGuild.channels.get(constants.tagbrobotChannelId);
+                tagbroBotChannel = oltpGuild.channels.get(constants.tagbrobotChannelId)
                 tagbroBotChannel.fetchMessage(constants.serverStatsMessageId)
                     .then(message => {
-                        existingServerStatsMessage = message;
-                        updateServerStatsMessage(existingServerStatsMessage);
+                        existingServerStatsMessage = message
+                        updateServerStatsMessage(existingServerStatsMessage)
                     })
-                    .catch(err);
+                    .catch(err)
             }
         }
-        let minutes = 2, the_interval = minutes * 60 * 1000;
+        let minutes = 2, the_interval = minutes * 60 * 1000
         setInterval(function () {
             if (existingServerStatsMessage !== undefined) {
-                updateServerStatsMessage(existingServerStatsMessage);
+                updateServerStatsMessage(existingServerStatsMessage)
             }
-        }, the_interval);
-    }
-};
+        }, the_interval)
+    },
+}
 
 async function getSortedServerStats() {
-    let serverStats = {};
-    for (let i = 0; i < constants.servers.length; i++) {
-        let server = constants.servers[i];
-        let address = constants.serverAddresses[server];
-        let response;
-        try {
-            response = await axios["get"](address + "stats");
-            let data = response.data;
-            serverStats[server] = [data["players"], data["games"]];
-        } catch (error) {
-            err("Failed axios get.");
-        }
+    try {
+        const response = await axios['get'](`${constants.tpa}stats`)
+        const data = response.data
+        return constants.servers.map(server => [server, data[server].ingame, data[server].games])
+    } catch (error) {
+        err('Failed axios get.')
+        return []
     }
-    return sortServerStats(serverStats);
 }
 
 function updateServerStatsMessage(existingServerStatsMessage) {
-    log("Updating server counts message.");
+    log('Updating server counts message.')
     module.exports.getSortedServerStatsMessage().then(response => {
-        let sortedServerStatsMessage = response;
-        let diamSplitIndex = sortedServerStatsMessage.indexOf(">\n") + ">\n".length;
-        let newServerStatsMessage = "Server counts:\n\n";
-        newServerStatsMessage = newServerStatsMessage.concat(sortedServerStatsMessage.slice(0, diamSplitIndex));
-        newServerStatsMessage = newServerStatsMessage.concat("\n*Other servers:*\n");
-        newServerStatsMessage = newServerStatsMessage.concat(sortedServerStatsMessage.slice(diamSplitIndex));
+        let sortedServerStatsMessage = response
+        let diamSplitIndex = sortedServerStatsMessage.indexOf('>\n') + '>\n'.length
+        let newServerStatsMessage = 'Server counts:\n\n'
+        newServerStatsMessage = newServerStatsMessage.concat(sortedServerStatsMessage.slice(0, diamSplitIndex))
+        newServerStatsMessage = newServerStatsMessage.concat('\n*Other servers:*\n')
+        newServerStatsMessage = newServerStatsMessage.concat(sortedServerStatsMessage.slice(diamSplitIndex))
         newServerStatsMessage = newServerStatsMessage.concat(
-            "\n*updated every 2 minutes. use `" + config.prefix + commandsInfo.commands.server_stats.alias +"` to manually check.*"
-        );
-        existingServerStatsMessage.edit(newServerStatsMessage);
-    }).catch(err);
+            '\n*updated every 2 minutes. use `' + config.prefix + commandsInfo.commands.server_stats.alias + '` to manually check.*',
+        )
+        existingServerStatsMessage.edit(newServerStatsMessage)
+    }).catch(err)
 }
 
 function sortServerStats(serverStats) {
-    let sortedServerStats = [];
+    let sortedServerStats = []
     for (let i = 0; i < constants.servers.length; i++) {
-        let server = constants.servers[i];
-        let serverData = serverStats[server];
-        sortedServerStats.push([server, serverData[0], serverData[1]]);
+        let server = constants.servers[i]
+        let serverData = serverStats[server]
+        sortedServerStats.push([server, serverData[0], serverData[1]])
     }
-    return sortedServerStats;
+    return sortedServerStats
 }
 
 function formatServerStats(serverStats, playerCountPadSize, gameCountPadSize) {
-    let server = serverStats[0];
-    let serverPlayers = serverStats[1];
-    let serverGames = serverStats[2];
-    let playersWord = "player";
-    let gamesWord = "game";
+    let server = serverStats[0]
+    let serverPlayers = serverStats[1]
+    let serverGames = serverStats[2]
+    let playersWord = 'player'
+    let gamesWord = 'game'
     if (serverPlayers !== 1) {
-        playersWord = playersWord.concat("s");
+        playersWord = playersWord.concat('s')
     } else {
-        playersWord = playersWord.concat(constants.spaceZws);
+        playersWord = playersWord.concat(constants.spaceZws)
     }
     if (serverGames !== 1) {
-        gamesWord = gamesWord.concat("s");
+        gamesWord = gamesWord.concat('s')
     } else {
-        gamesWord = gamesWord.concat(constants.spaceZws);
+        gamesWord = gamesWord.concat(constants.spaceZws)
     }
-    return "`" + utils.pad(" ".repeat(10), server + ":", false) +
-        utils.pad(" ".repeat(playerCountPadSize), serverPlayers, true) + " " + playersWord + " and " +
-        utils.pad(" ".repeat(gameCountPadSize), serverGames, true) + " " + gamesWord + "`";
+    return '`' + utils.pad(' '.repeat(10), server + ':', false) +
+        utils.pad(' '.repeat(playerCountPadSize), serverPlayers, true) + ' ' + playersWord + ' and ' +
+        utils.pad(' '.repeat(gameCountPadSize), serverGames, true) + ' ' + gamesWord + '`'
 }
 
 function getLargestPlayerCount(serverStats) {
     return getLargestServerStat(serverStats, 1)
 }
+
 function getLargestGameCount(serverStats) {
     return getLargestServerStat(serverStats, 2)
 }
+
 function getLargestServerStat(serverStats, index) {
-    let maxSoFar = -1;
+    let maxSoFar = -1
     for (let i = 0; i < serverStats.length; i++) {
-        let serverStat = serverStats[i];
+        let serverStat = serverStats[i]
         for (let j = 0; j < serverStat.length; j++) {
-            maxSoFar = Math.max(maxSoFar, serverStat[index]);
+            maxSoFar = Math.max(maxSoFar, serverStat[index])
         }
     }
-    return maxSoFar;
+    return maxSoFar
 }
